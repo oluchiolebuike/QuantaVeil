453d04dc620b9e5df22e422ae723fc44526edbce
Oluchi Aviwe <oluchiolebuike@gmail.com>
2026-05-15 23:52:15 +0200

    Update file header with last modified date
    Added last modified date to the file header.

--- qec-simulator/qec_sim.py
+++ qec-simulator/qec_sim.py
@@ -1,5 +1,6 @@
 # Author            : Oluchi Aviwe Olebuike
 # Date Created      : 09/05/2026
+# Last modified     : 15/05/2026
 # Project title     : Quantum Algorithm Noise Analysis using QEC-Inspired Models
 # File name         : qec_sim.py
 # Modernised        : Qiskit 2.x / qiskit-aer 0.17+
