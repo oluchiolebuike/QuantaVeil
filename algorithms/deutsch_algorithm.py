c518ae2ee0d152a4d4aeaf58aa6ca5711d12e2f0
Oluchi Aviwe <oluchiolebuike@gmail.com>
2026-05-15 23:16:10 +0200

    Revise metadata and installation instructions
    Updated author information and installation instructions.

--- algorithms/deutsch_algorithm.py
+++ algorithms/deutsch_algorithm.py
@@ -26,7 +26,7 @@
 # Algorithm stability under increasing decoherence
 
 # Installations:
-# pip install qiskit qiskit-aer matplotlib numpy pandas seaborn
+# pip install qiskit qiskit-aer matplotlib numpy pandas seaborn pylatexenc
 
 from qiskit import QuantumCircuit
 
