0411d29b6db8fa7e67abce0b05957ef6c6400ffe
Oluchi Aviwe <oluchiolebuike@gmail.com>
2026-05-15 22:36:44 +0200

    Rename file header

--- core/noise_models.py
+++ core/noise_models.py
@@ -1,7 +1,7 @@
 # Author            : Oluchi Aviwe Olebuike
 # Date Created      : 09/05/2026
 # Project title     : Quantum Algorithm Noise Analysis using QEC-Inspired Models
-# File name         : grovers_algorithm.py
+# File name         : noise_models.py
 # Modernised        : Qiskit 2.x / qiskit-aer 0.17+
 
 
